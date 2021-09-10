73a916e4b596aad08bc506ae43e7186215ae4165
Pulkit007 <77740386+Pulkit007@users.noreply.github.com>
2021-09-10 20:23:01 +0530

    deployed

--- todolist/client/src/api/api.jsx
+++ todolist/client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todolist-pulkitb.herokuapp.com/api",
 });
 
 export const createTask = (payload) => api.post("/task/add", payload);
